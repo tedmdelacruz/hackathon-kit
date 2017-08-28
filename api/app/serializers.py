33a9692c11aa4f972f6f5c7dbe72b12e1b04b953
Ted Mathew dela Cruz <tedmdelacruz@gmail.com>
2017-08-28 16:19:09 +0800

    Set initial fields for User serializer

--- api/app/serializers.py
+++ api/app/serializers.py
@@ -2,9 +2,10 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.Serializer):
+class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active',
+            'is_superuser', 'last_login', 'date_joined')
 
