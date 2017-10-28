6441df938268b405efda577bf39beb7f70837541
Ted Mathew dela Cruz <tedmdelacruz@gmail.com>
2017-10-28 17:55:20 +0800

    Setup netcat

--- server/bootstrap.sh
+++ server/bootstrap.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+
+# Wait for db service to be ready using netcat
+nc -z db 3306
+n=$?
+while [ $n -ne 0 ]; do
+    sleep 1
+    nc -z db 3306
+    n=$?
+done
+
 python3 manage.py runserver 0.0.0.0:8000
