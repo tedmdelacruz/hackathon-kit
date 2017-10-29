9f302e249e03e1820c0ba3337202defd62664423
Ted Mathew dela Cruz <tedmdelacruz@gmail.com>
2017-10-29 12:13:54 +0800

    Fix user registration

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -6,5 +6,9 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active',
+        fields = ('id', 'username', 'password', 'email', 'first_name', 'last_name', 'is_staff', 'is_active',
             'is_superuser', 'last_login', 'date_joined')
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
