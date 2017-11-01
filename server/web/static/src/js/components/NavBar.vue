bef45ca24aceaf6ac06abfc635a2f3a2bb0b254e
Ted Mathew dela Cruz <tedmdelacruz@gmail.com>
2017-11-01 23:36:33 +0800

    Improve app style

--- server/web/static/src/js/components/NavBar.vue
+++ server/web/static/src/js/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="navbar navbar-expand-lg">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand" href="/"><strong>App</strong></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -16,7 +16,7 @@
             </ul>
             <form class="form-inline my-2 my-lg-0">
                 <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit">Search</button>
             </form>
         </div>
     </nav>
--- server/web/static/src/js/components/UsersTable.vue
+++ server/web/static/src/js/components/UsersTable.vue
@@ -1,14 +1,21 @@
 <template>
-    <table class="table table-condensed table-hover">
+    <table class="table table-condensed table-dark table-hover">
         <thead>
+            <th>Name</th>
             <th>Username</th>
             <th>Email</th>
+            <th>Status</th>
             <th></th>
         </thead>
         <tbody>
-            <tr v-for="user in this.$store.state.users">
+            <tr v-for="user in this.$store.state.users"
+                v-bind:class="{ 'text-secondary': !user.is_active }">
+                <td>{{ user.first_name + ' ' + user.last_name}}</td> 
                 <td>{{ user.username }}</td>
                 <td>{{ user.email }}</td>
+                <td>
+                    {{ user.is_active ? 'Active' : 'Inactive' }}
+                </td>
                 <td>-</td>
             </tr>
         </tbody>
