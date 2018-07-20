50915513e83da2e037615d2e2a8355376e634cdd
Ted Mathew dela Cruz <tedmdelacruz@gmail.com>
2018-07-21 01:09:20 +0800

    Add caching strategy for users API calls

--- src/app/web/static/sw.js
+++ src/app/web/static/sw.js
@@ -1,16 +1,25 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.3.1/workbox-sw.js');
 
-workbox.routing.registerRoute(
-  '/#/',
-  workbox.strategies.networkFirst({
-    cacheName: 'index',
-  }),
-);
-
+// Assets
 // https://developers.google.com/web/tools/workbox/guides/common-recipes
 workbox.routing.registerRoute(
   /\.(?:js|css)$/,
   workbox.strategies.staleWhileRevalidate({
     cacheName: 'static-resources',
+  }),
+);
+
+// App-specific
+workbox.routing.registerRoute(
+  '/#/',
+  workbox.strategies.cacheFirst({
+    cacheName: 'index',
+  }),
+);
+
+workbox.routing.registerRoute(
+  '/api/users/',
+  workbox.strategies.networkFirst({
+    cacheName: 'users',
   }),
 );
